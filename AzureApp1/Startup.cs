5fd5d59848d1aa267a5700c8d58ec13048e4c1a7
tien nguyen <tien.nguyen@idealogic.com.vn>
2017-07-12 15:41:41 +0700

    Start test projects for testing multi project on repo on azure deployment

---
+++ AzureApp1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureApp1.Startup))]
+namespace AzureApp1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ AzureApp2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureApp2.Startup))]
+namespace AzureApp2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
